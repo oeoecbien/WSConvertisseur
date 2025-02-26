ec5cdf808407e6ed31f25dad1f94dc87267cd9a3
melih0132 <melih.cetinkaya.32@gmail.com>
2025-02-26 20:16:24 +0100

    AJOUT CONTROLLER

---
+++ WSConvertisseur/Controllers/DevisesController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WSConvertisseur.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WSConvertisseur.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevisesController : ControllerBase
+    {
+        private List<Devise> devises;
+
+        public DevisesController()
+        {
+            devises = new List<Devise>();
+            devises.Add(new Devise(1, "Dollar", 1.08));
+            devises.Add(new Devise(2, "Franc Suisse", 1.07));
+            devises.Add(new Devise(3, "Yen", 120));
+        }
+
+        // GET: api/<DevisesController>
+        [HttpGet]
+        [ProducesResponseType(200)]
+        //public IEnumerable<string> Get()
+        public IEnumerable<Devise> GetAll()
+        {
+            //return new string[] { "value1", "value2" };
+            return devises;
+        }
+
+        /// <summary>
+        /// Get a single currency.
+        /// </summary>
+        /// <returns>Http response</returns>
+        /// <param name="id">The id of the currency</param>
+        /// <response code="200">When the currency id is found</response> 
+        /// <response code="404">When the currency id is not found</response> 
+        //public string Get(int id)
+        //public Devise GetById(int id)
+        // GET api/<DevisesController>/5
+        [HttpGet("{id}", Name = "GetDevise")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public ActionResult<Devise> GetById(int id)
+        {
+            Devise? devise = devises.FirstOrDefault((d) => d.Id == id);
+            if (devise == null)
+            {
+                return NotFound();
+            }
+            return devise;
+        }
+
+        // POST api/<DevisesController>
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        //public void Post([FromBody] string value)
+        public ActionResult<Devise> Post([FromBody] Devise devise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            devises.Add(devise);
+            return CreatedAtRoute("GetDevise", new { id = devise.Id }, devise);
+            //OU: return CreatedAtAction("GetById", new { id = devise.Id }, devise);
+
+        }
+
+        // PUT api/<DevisesController>/5
+        [HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, [FromBody] Devise devise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != devise.Id)
+            {
+                return BadRequest();
+            }
+            int index = devises.FindIndex((d) => d.Id == id);
+            if (index < 0)
+            {
+                return NotFound();
+            }
+            devises[index] = devise;
+            return NoContent();
+        }
+
+        // DELETE api/<DevisesController>/5
+        [HttpDelete("{id}")]
+        //public void Delete(int id)
+        public ActionResult<Devise> Delete(int id)
+        {
+            Devise? devise = devises.FirstOrDefault((d) => d.Id == id);
+            if (devise == null)
+            {
+                return NotFound();
+            }
+            devises.Remove(devise);
+            return devise;
+        }
+    }
+}
