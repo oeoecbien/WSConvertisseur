d627ec55886e7a4a38c17529dca5e331806816e6
melih0132 <melih.cetinkaya.32@gmail.com>
2025-02-26 20:10:51 +0100

    AJOUT MODEL

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+		private int id;
+		/// <summary>
+		/// Id de la devise
+		/// </summary>
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string? nomDevise;
+
+        /// <summary>
+        /// Nom de la devise
+        /// </summary>
+        [Required]
+        public string? NomDevise
+		{
+			get { return nomDevise; }
+			set { nomDevise = value; }
+		}
+
+		private double taux;
+
+        /// <summary>
+        /// Taux de la devise
+        /// </summary>public int Taux
+        public double Taux
+		{
+            get { return taux; }
+			set { taux = value; }
+		}
+
+        public Devise() {}
+
+        public Devise(int id, string? nomDevise, double taux)
+        {
+            Id = id;
+            NomDevise = nomDevise;
+            Taux = taux;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Devise devise &&
+                   Id == devise.Id &&
+                   NomDevise == devise.NomDevise &&
+                   Taux == devise.Taux;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, NomDevise, Taux);
+        }
+    }
+}
